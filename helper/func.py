998580a28c55f04bd3a7513557dcd09427c5591b
PeterPark07 <slimshadx69@gmail.com>
2023-06-09 21:11:21 +0530

    Update func.py

--- helper/func.py
+++ helper/func.py
@@ -2,10 +2,9 @@ from telegraph import Telegraph
 
 telegraph = Telegraph()
 
-
 def login(token):
     global telegraph
-    telegraph = Telegraph(access_token = token)
+    telegraph = Telegraph(access_token=token)
     return f"Logged in successfully.\n{get_account_info()}"
 
 def create_account(short_name, author_name=None, author_url=None, replace_token=True):
@@ -15,27 +14,21 @@ def create_account(short_name, author_name=None, author_url=None, replace_token=
         author_url=author_url,
         replace_token=replace_token
     )
-    
-    response = ''
-    for data in account:
-        response+= f"{data} -> {account[data]}\n"
-    return response
+    return format_response(account)
 
 def get_account_info():
     account = telegraph.get_account_info(['short_name', 'author_name', 'author_url', 'auth_url', 'page_count'])
-    response = ''
-    for data in account:
-        response+= f"{data} -> {account[data]}\n"
-    return response
+    return format_response(account)
 
 def create_page(title, html_content, author_name=None, author_url=None, return_content=True):
-    return telegraph.create_page(
+    page = telegraph.create_page(
         title=title,
         html_content=html_content,
         author_name=author_name,
         author_url=author_url,
         return_content=return_content
     )
+    return format_response(page)
 
 def edit_account_info(short_name=None, author_name=None, author_url=None):
     return telegraph.edit_account_info(
@@ -57,9 +50,9 @@ def edit_page(path, title, html_content=None, author_name=None, author_url=None,
 def get_access_token():
     token = telegraph.get_access_token()
     response = f"Your access token is ->> {token}"
-    response+= f"\n\nTo login using this token, use \n/login_{token}"
+    response += f"\n\nTo login using this token, use \n/login_{token}"
     return response
-    
+
 def get_page(path, return_content=True, return_html=True):
     return telegraph.get_page(
         path=path,
@@ -70,15 +63,12 @@ def get_page(path, return_content=True, return_html=True):
 def get_page_list(offset=0, limit=200):
     pages = telegraph.get_page_list(offset=offset, limit=limit)
     num_pages = pages["total_count"]
-    count = 0
     result = ''
-    for page in pages["pages"]:
-        count+= 1
-        result+= "Page {count}\n\n"
-        for data in page:
-            result+= f"{data} -> {page[data]}\n"
+    for count, page in enumerate(pages["pages"], 1):
+        result += f"Page {count}\n\n"
+        result += format_response(page)
     return result
-    
+
 def get_views(path, year=None, month=None, day=None, hour=None):
     return telegraph.get_views(
         path=path,
@@ -90,10 +80,13 @@ def get_views(path, year=None, month=None, day=None, hour=None):
 
 def revoke_access_token():
     new_token = telegraph.revoke_access_token()
-    result = ''
-    for data in new_token:
-        result+= f"{data} -> {new_token[data]}\n"
-    return result
-    
+    return format_response(new_token)
+
 def upload_file(f):
     return telegraph.upload_file(f)
+
+def format_response(data):
+    response = ''
+    for key, value in data.items():
+        response += f"{key} -> {value}\n"
+    return response

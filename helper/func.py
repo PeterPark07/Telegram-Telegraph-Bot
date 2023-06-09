9a2a9287acf74a6ea5eb646dcdb7ed7a8da1f583
PeterPark07 <slimshadx69@gmail.com>
2023-06-09 19:33:59 +0530

    n

--- helper/func.py
+++ helper/func.py
@@ -2,8 +2,6 @@ from telegraph import Telegraph
 
 telegraph = Telegraph()
 
-def get_account_info():
-    return telegraph.get_account_info(['short_name', 'author_name', 'author_url', 'auth_url', 'page_count'])
 
 def login(token):
     global telegraph
@@ -11,12 +9,24 @@ def login(token):
     return get_account_info()
 
 def create_account(short_name, author_name=None, author_url=None, replace_token=True):
-    return telegraph.create_account(
+    account = telegraph.create_account(
         short_name=short_name,
         author_name=author_name,
         author_url=author_url,
         replace_token=replace_token
     )
+    
+    response = ''
+    for data in account:
+        response+= f"{data} -> {account[data]}\n"
+    return response
+
+def get_account_info():
+    account = telegraph.get_account_info(['short_name', 'author_name', 'author_url', 'auth_url', 'page_count'])
+    response = ''
+    for data in account:
+        response+= f"{data} -> {account[data]}\n"
+    return response
 
 def create_page(title, html_content, author_name=None, author_url=None, return_content=True):
     return telegraph.create_page(
@@ -45,8 +55,11 @@ def edit_page(path, title, html_content=None, author_name=None, author_url=None,
     )
 
 def get_access_token():
-    return telegraph.get_access_token()
-
+    token = telegraph.get_access_token()
+    response = f"Your access token is ->> {token}"
+    response+= f"\n\nTo login using this token, use \n/login_{token}"
+    return response
+    
 def get_page(path, return_content=True, return_html=True):
     return telegraph.get_page(
         path=path,
@@ -55,8 +68,17 @@ def get_page(path, return_content=True, return_html=True):
     )
 
 def get_page_list(offset=0, limit=200):
-    return telegraph.get_page_list(offset=offset, limit=limit)
-
+    pages = telegraph.get_page_list(offset=offset, limit=limit)
+    num_pages = pages["total_count"]
+    count = 0
+    result = ''
+    for page in pages["pages"]:
+        count+= 1
+        result+= "Page {count}\n\n"
+        for data in page:
+            result+= f"{data} -> {page[data]}\n"
+    return result
+    
 def get_views(path, year=None, month=None, day=None, hour=None):
     return telegraph.get_views(
         path=path,
@@ -67,7 +89,11 @@ def get_views(path, year=None, month=None, day=None, hour=None):
     )
 
 def revoke_access_token():
-    return telegraph.revoke_access_token()
-
+    new_token = telegraph.revoke_access_token()
+    result = ''
+    for data in new_token:
+        result+= f"{data} -> {new_token[data]}\n"
+    return result
+    
 def upload_file(f):
     return telegraph.upload_file(f)
--- src/main.py
+++ src/main.py
@@ -45,38 +45,30 @@ def handle_existing_account(message):
         result = "Invalid access token provided"
     bot.reply_to(message, result)
 
+
 @bot.message_handler(commands=['create_account'])
 def handle_account_creation(message):
     global account0
-    # Handle the /start command
-    input = message.text
-    
-    input = input.split()
+    input = message.text.split()
     
     if len(input) == 2:
-        account = create_account(input[1])
-        response = 'short_name - ' + account.get('short_name') + '\naccess_token - ' + account.get('access_token') + '\nauth_url - ' + account.get('auth_url')
+        response = create_account(input[1])
         bot.reply_to(message, response)
         account0 = True
         
     elif len(input) == 3:
-        account = create_account(input[1], input[2])
-        response = 'short_name - ' + account.get('short_name') + '\nauthor_name - ' + account.get('author_name') + '\naccess_token - ' + account.get('access_token') + '\nauth_url - ' + account.get('auth_url')
+        response = create_account(input[1], input[2])
         bot.reply_to(message, response)
         account0 = True
     else:
         bot.reply_to(message, "Usage - /create_account <short_name> <author_name(optional)>")
-
-
+        
     
 @bot.message_handler(commands=['my_account'])
 def handle_account_info(message):
     # Handle the /start command
     if account0:
-        info = get_account_info()
-        result = ''
-        for i in info:
-            result += f"{i} -> {info.get(i)}\n"
+        result = get_account_info()
     else:
         result = "Not logged in."
     bot.reply_to(message, result)
@@ -85,8 +77,7 @@ def handle_account_info(message):
 @bot.message_handler(commands=['get_access_token'])
 def handle_access_token(message):
     if account0:
-        result = f"Your access token is ->> {get_access_token()}"
-        result+= f"\n\nTo login using this token, use \n/login_{get_access_token()}"
+        result = get_access_token()
     else:
         result = "Not logged in."
     bot.reply_to(message, result)
@@ -95,15 +86,7 @@ def handle_access_token(message):
 @bot.message_handler(commands=['get_page_list'])
 def handle_page_list(message):
     if account0:
-        pages = get_page_list()
-        num_pages = pages["total_count"]
-        count = 0
-        result = ''
-        for i in pages["pages"]:
-            count+= 1
-            result+= "Page {count}\n\n"
-            for j in i:
-                result+= f"{j} -> {i[j]}\n"
+        result = get_page_list()
         if result == '':
             result = "No pages found."
     else:
@@ -115,10 +98,7 @@ def handle_page_list(message):
 @bot.message_handler(commands=['revoke_access_token'])
 def handle_revoke_token(message):
     if account0:
-        new_token = revoke_access_token()
-        result = ''
-        for i in new_token:
-            result+= f"{i} -> {new_token[i]}\n"
+        result = revoke_access_token()
     else:
         result = "Not logged in."
     bot.reply_to(message, result)
